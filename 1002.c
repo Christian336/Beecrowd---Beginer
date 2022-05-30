35706fbe7970602b4157fd2d4c3cff8d7a1dc13b
Christian dos Santos <105569932+Christian336@users.noreply.github.com>
2022-05-30 00:10:43 +0000

    Initial commit

---
+++ 1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+  double A, R;
+double p= 3.14159;
+
+scanf("%lf" , &R);
+
+  A = p * pow(R, 2);
+
+ printf("A=%0.4lf\n" , A); 
+
+
+  return 0;
+}
---
+++ 1004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int x, y, PROD;
+
+  scanf("%d\n%d" , &x , &y);
+
+  PROD = x * y;
+
+  printf("PROD = %d\n" , PROD);
+
+  
+
+
+  return 0;
+}
